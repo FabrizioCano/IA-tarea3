c2603c4f03e9b6a2cdff6dd397cd9d58e68ab473
FabrizioCano <fabriziocano7@gmail.com>
2025-05-24 14:11:03 -0300

    first commit

---
+++ TSP.py
@@ -0,0 +1,58 @@
+import numpy as np
+import random as rand
+
+class TSP:
+    def __init__(self,filename):
+        self.n_cities=0
+        self.n_objectives=0
+        self.adx_matrix=[]
+        self.filename=filename
+        self._load_file()
+        
+    def _load_file(self):
+        with open(self.filename,'r') as file:
+            lines=[line.strip() for line in file.readlines() if line.strip() != '']
+        
+        """ establecer el numero de ciudades y objetivos """
+        self.n_cities=int(lines[0])
+        self.n_objectives=int(lines[1])
+        
+        """ leer desde la linea 2 """
+        line=2
+        
+        """ leer por cada objetivo una matriz, la matriz de adjayencia sera una matriz de matrices donde cada una representa a un objetivo """
+        for i in range(self.n_objectives):
+            mat=[]
+            
+            for j in range(self.n_cities):
+                row=list(map(float,lines[line].split()))
+                mat.append(row)
+                line+=1
+            self.adx_matrix.append(np.array(mat))
+            
+    def evaluar(self,tour):
+        """ evalua un camino y retorna una tupla con los costos asociados a este camino """
+        costos=[]
+        for mat in self.adx_matrix:
+            cost=0
+            
+            for j in range(len(tour)):
+                """ nos aseguramos de formar un ciclo hamiltoniano con el camnio, empieza en la primera ciudad y termina en esa misma """
+                cost+=mat[tour[j]] [tour[(j+1)%len(tour)]]
+            costos.append(cost)
+        return tuple(costos)
+
+    def print_summary(self):
+        print(f"Number of cities: {self.n_cities}")
+        print(f"Number of objectives: {self.n_objectives}")
+        for i, matrix in enumerate(self.adx_matrix):
+            print(f"Objective {i+1} distance matrix (shape={matrix.shape}) loaded.")
+
+tsp1=TSP("tsp_KROAB100.TSP.TXT")
+tsp1.print_summary()
+
+for i in range(100):
+    tour = list(range(tsp1.n_cities))
+    rand.shuffle(tour)
+    cost = tsp1.evaluar(tour)
+    print(f'Tour {i+1} cost (ambos objetivos): ({cost[0]:.2f}, {cost[1]:.2f})')
